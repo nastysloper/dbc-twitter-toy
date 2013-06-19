f15a339b49e2a945e5d16f0a89c54a5983b34c7a
Lillie Chilen <lillie.chilen@gmail.com>
2013-06-19 10:15:56 -0700

    Cache tweets per user

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,7 +7,14 @@ get '/tweets' do
 end
 
 get '/:username' do |username|
-  @tweets = fresh_tweets(username)
-  @tweeter = username
+  @user = TwitterUser.find_or_create_by_username(username)
+  if @user.tweets.empty?
+    fresh_tweets = fresh_tweets(username)
+    fresh_tweets.each do |tweet|
+      Tweet.create(tweet: tweet, twitter_user: @user)
+    end
+  end
+
+  @tweets = @user.tweets.limit(10)
   erb :tweets
 end
