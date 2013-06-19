15db6d6ffc0524b546c393948ba5aafb5c18fbf1
Lillie Chilen <lillie.chilen@gmail.com>
2013-06-19 10:47:49 -0700

    WIP - add tweet freshness logic

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,11 +8,10 @@ end
 
 get '/:username' do |username|
   @user = TwitterUser.find_or_create_by_username(username)
-  if @user.tweets.empty?
+  if @user.tweets_stale?
+    puts @user.tweets_stale?
     fresh_tweets = fresh_tweets(username)
-    fresh_tweets.each do |tweet|
-      Tweet.create(tweet: tweet, twitter_user: @user)
-    end
+    @user.tweets_last_pulled = Time.now
   end
 
   @tweets = @user.tweets.limit(10)
--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -1,3 +1,13 @@
 class TwitterUser < ActiveRecord::Base
   has_many :tweets
+
+  def tweets_stale?
+    self.tweets_last_pulled ? cache_age(self) : true
+  end
+
+  private
+    def cache_age(user)
+      cache_age = (Time.now - user.tweets_last_pulled)/60
+      return cache_age > 1
+    end
 end
